7f4b09b3f19535d899135b3e3234872acfd629d2
Francisca Leon Labarca <francisca.leon@dpp.cl>
2023-09-12 18:01:22 -0300

    status

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AppComponent } from '../app.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginPage implements OnInit {
   formularioLogin: FormGroup;
   
 
-  constructor(public fb:FormBuilder, private appComponent: AppComponent) { 
+  constructor(public fb:FormBuilder, private appComponent: AppComponent, private router: Router) { 
     this.formularioLogin = this.fb.group({
       'usuario': new FormControl('', Validators.required),
       'clave': new FormControl('', Validators.required)
@@ -25,6 +26,18 @@ export class LoginPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  async guardar(){
+    var f = this.formularioLogin.value;
+
+    var login = {
+      usuario: f.usuario,
+      clave: f.clave
+    }
+
+    localStorage.setItem('login', JSON.stringify(login));
+    this.router.navigate(['/inicio']);
   }
 
 }
