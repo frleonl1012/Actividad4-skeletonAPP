9ca2cbf0a1f1513978bc02c066d8c7ee7c3511ac
Francisca Leon Labarca <francisca.leon@dpp.cl>
2023-09-12 17:24:12 -0300

    contenido login

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-inicio',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor(private appComponent: AppComponent) { }
+
+  ImagenLogo(): string {
+    return this.appComponent.imgLogo;
+  }
 
   ngOnInit() {
   }
